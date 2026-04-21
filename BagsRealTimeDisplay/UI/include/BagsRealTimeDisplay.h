7fc693710adec8c873a1ff766b90064c67dce163
zfkj4090 <zfkj4090@DESKTOP-GJ77VA6>
2026-04-21 16:40:45 +0800

    重构主界面UI，新增关机/重启弹窗功能
    - 新增 DlgCloseForm 关机/重启/退出弹窗，支持跨平台关机重启
    - 增加可点击标题控件 rqw_LabelClickable
    - 主界面 UI 大幅重构，优化布局与样式，增加相机状态、调试模式、剔除功能、公司信息、设置等控件
    - 主界面代码适配 UI 变更，完善信号槽、控件初始化、图片与状态显示
    - 新增关闭按钮图标资源，修正 CMake 配置

--- BagsRealTimeDisplay/UI/include/BagsRealTimeDisplay.h
+++ BagsRealTimeDisplay/UI/include/BagsRealTimeDisplay.h
@@ -3,8 +3,11 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_BagsRealTimeDisplay.h"
 
+#include "rqw_LabelClickable.h"
+#include "DlgCloseForm.h"
+
 QT_BEGIN_NAMESPACE
-namespace Ui { class BagsRealTimeDisplay; };
+namespace Ui { class BagsRealTimeDisplayClass; };
 QT_END_NAMESPACE
 
 class BagsRealTimeDisplay : public QMainWindow
@@ -14,8 +17,32 @@ class BagsRealTimeDisplay : public QMainWindow
 public:
     BagsRealTimeDisplay(QWidget* parent = nullptr);
     ~BagsRealTimeDisplay();
+public:
+	void initializeComponents();
+public:
+	void build_ui();
+	void build_connect();
+	void build_BagsRealTimeDisplayData();
+	void ini_clickableTitle();
+	void build_DlgCloseForm();
+public:
+	void build_camera();
+public slots:
+	void updateCameraLabelState(int cameraIndex, bool state);
 
+    void onCameraDisplay(size_t index, QPixmap image);
+
+	void lb_title_clicked();
+private slots:
+	void pbtn_exit_clicked();
+	void pbtn_set_clicked();
+	void rbtn_debug_checked(bool checked);
+	void rbtn_removeFunc_checked(bool checked);
 private:
-    Ui::BagsRealTimeDisplay* ui;
+	rw::rqw::ClickableLabel* clickableTitle = nullptr;
+	int minimizeCount{ 3 };
+	DlgCloseForm* _dlgCloseForm = nullptr;
+private:
+    Ui::BagsRealTimeDisplayClass* ui;
 };
 
--- BagsRealTimeDisplay/UI/src/BagsRealTimeDisplay.cpp
+++ BagsRealTimeDisplay/UI/src/BagsRealTimeDisplay.cpp
@@ -1,11 +1,153 @@
 #include "BagsRealTimeDisplay.h"
 
 BagsRealTimeDisplay::BagsRealTimeDisplay(QWidget* parent)
+	: QMainWindow(parent)
+	, ui(new Ui::BagsRealTimeDisplayClass())
+{
+	ui->setupUi(this);
+
+	initializeComponents();
+}
+
+BagsRealTimeDisplay::~BagsRealTimeDisplay()
+{
+	
+}
+
+void BagsRealTimeDisplay::initializeComponents()
+{
+	build_ui();
+
+	build_camera();
+
+	build_connect();
+}
+
+void BagsRealTimeDisplay::build_ui()
+{
+	build_BagsRealTimeDisplayData();
+	build_DlgCloseForm();
+}
+
+void BagsRealTimeDisplay::build_connect()
+{
+	QObject::connect(ui->pbtn_exit, &QPushButton::clicked,
+		this, &BagsRealTimeDisplay::pbtn_exit_clicked);
+	QObject::connect(ui->pbtn_set, &QPushButton::clicked,
+		this, &BagsRealTimeDisplay::pbtn_set_clicked);
+	QObject::connect(ui->rbtn_debug, &QRadioButton::clicked,
+		this, &BagsRealTimeDisplay::rbtn_debug_checked);
+	QObject::connect(ui->rbtn_removeFunc, &QRadioButton::clicked,
+		this, &BagsRealTimeDisplay::rbtn_removeFunc_checked);
+	// 连接显示标题
+	QObject::connect(clickableTitle, &rw::rqw::ClickableLabel::clicked,
+		this, &BagsRealTimeDisplay::lb_title_clicked);
+}
+
+void BagsRealTimeDisplay::build_BagsRealTimeDisplayData()
 {
 
 }
 
-BagsRealTimeDisplay::~BagsRealTimeDisplay()
+void BagsRealTimeDisplay::ini_clickableTitle()
+{
+	// 初始化标题label
+	clickableTitle = new rw::rqw::ClickableLabel(this);
+	auto layoutTitle = ui->groupBox_head->layout();
+	layoutTitle->replaceWidget(ui->label_title, clickableTitle);
+	delete ui->label_title;
+	clickableTitle->setText("钉子检测");
+	clickableTitle->setStyleSheet("QLabel {font-size: 30px;font-weight: bold;color: rgb(255, 255, 255);padding: 5px 5px;border-bottom: 2px solid #cccccc;}");
+}
+
+void BagsRealTimeDisplay::build_DlgCloseForm()
+{
+	_dlgCloseForm = new DlgCloseForm(this);
+}
+
+void BagsRealTimeDisplay::build_camera()
+{
+
+}
+
+void BagsRealTimeDisplay::updateCameraLabelState(int cameraIndex, bool state)
+{
+	switch (cameraIndex)
+	{
+	case 1:
+		if (state) {
+			ui->label_camera1State->setText("连接成功");
+			ui->label_camera1State->setStyleSheet(QString("QLabel{color:rgb(0, 230, 0);font-size: 18px;font - weight: bold;padding: 5px 5px;} "));
+		}
+		else {
+			ui->label_camera1State->setText("连接失败");
+			ui->label_camera1State->setStyleSheet(QString("QLabel{color:rgb(230, 0, 0);font-size: 18px;font - weight: bold;padding: 5px 5px;} "));
+		}
+		break;
+	case 2:
+		if (state) {
+			ui->label_camera2State->setText("连接成功");
+			ui->label_camera2State->setStyleSheet(QString("QLabel{color:rgb(0, 230, 0);font-size: 18px;font - weight: bold;padding: 5px 5px;} "));
+		}
+		else {
+			ui->label_camera2State->setText("连接失败");
+			ui->label_camera2State->setStyleSheet(QString("QLabel{color:rgb(230, 0, 0);font-size: 18px;font - weight: bold;padding: 5px 5px;} "));
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+void BagsRealTimeDisplay::onCameraDisplay(size_t index, QPixmap image)
+{
+	if (1 == index)
+	{
+		ui->label_imgDisplay_1->setPixmap(image.scaled(ui->label_imgDisplay_1->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+	}
+	else if (2 == index)
+	{
+		ui->label_imgDisplay_2->setPixmap(image.scaled(ui->label_imgDisplay_2->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+	}
+}
+
+void BagsRealTimeDisplay::lb_title_clicked()
+{
+	if (0 != minimizeCount)
+	{
+		minimizeCount--;
+	}
+	else if (0 >= minimizeCount)
+	{
+		// 最小化主窗体
+		this->showMinimized();
+		minimizeCount = 3; // 重置最小化计数器
+	}
+}
+
+void BagsRealTimeDisplay::pbtn_exit_clicked()
+{
+#ifdef NDEBUG
+	if (_dlgCloseForm)
+	{
+		_dlgCloseForm->exec();
+	}
+#else
+	this->close();
+#endif
+}
+
+void BagsRealTimeDisplay::pbtn_set_clicked()
+{
+
+}
+
+void BagsRealTimeDisplay::rbtn_debug_checked(bool checked)
+{
+
+}
+
+void BagsRealTimeDisplay::rbtn_removeFunc_checked(bool checked)
 {
 
 }
