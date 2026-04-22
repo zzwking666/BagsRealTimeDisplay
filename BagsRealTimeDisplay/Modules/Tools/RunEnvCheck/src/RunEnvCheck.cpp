b750de42dca6a4ddea634333b6b1f3d6f90c4e47
zfkj4090 <zfkj4090@DESKTOP-GJ77VA6>
2026-04-22 14:40:27 +0800

    将 RunEnvCheck 移至全局命名空间并优化结构
    将 RunEnvCheck 类从嵌套命名空间移至全局，移除相关命名空间代码。统一并格式化类的声明与实现，模板函数实现移至全局，优化代码结构和可读性。

--- BagsRealTimeDisplay/Modules/Tools/RunEnvCheck/src/RunEnvCheck.cpp
+++ BagsRealTimeDisplay/Modules/Tools/RunEnvCheck/src/RunEnvCheck.cpp
@@ -4,30 +4,26 @@
 #include<QSharedMemory>
 #include<QFile>
 
-namespace rw {
-	namespace rqw {
-		bool RunEnvCheck::isSingleInstance(const QString& instanceName)
-		{
-			static QSharedMemory sharedMemory(instanceName);
-			if (!sharedMemory.create(1)) {
-				return false; 
-			}
-			return true; 
-		}
-
-		bool RunEnvCheck::isProcessRunning(const QString& processName)
-		{
-			QProcess process;
-			process.start("tasklist", QStringList() << "/FI" << QString("IMAGENAME eq %1").arg(processName));
-			process.waitForFinished();
-			QString output = process.readAllStandardOutput();
-			return output.contains(processName, Qt::CaseInsensitive);
-		}
-
-		bool RunEnvCheck::isFileExist(const QString& filePath)
-		{
-			QFile file(filePath);
-			return file.exists();
-		}
+bool RunEnvCheck::isSingleInstance(const QString& instanceName)
+{
+	static QSharedMemory sharedMemory(instanceName);
+	if (!sharedMemory.create(1)) {
+		return false;
 	}
+	return true;
+}
+
+bool RunEnvCheck::isProcessRunning(const QString& processName)
+{
+	QProcess process;
+	process.start("tasklist", QStringList() << "/FI" << QString("IMAGENAME eq %1").arg(processName));
+	process.waitForFinished();
+	QString output = process.readAllStandardOutput();
+	return output.contains(processName, Qt::CaseInsensitive);
+}
+
+bool RunEnvCheck::isFileExist(const QString& filePath)
+{
+	QFile file(filePath);
+	return file.exists();
 }
